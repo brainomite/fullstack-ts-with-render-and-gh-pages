e30f86c95735d9db1b7a384e1fcdc20ff35e166f
Aaron Young <hi@aaronyoung.dev>
2024-09-11 10:41:14 -0400

    feat: add link to helloworld

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -3,6 +3,7 @@ import reactLogo from "../assets/react.svg";
 import viteLogo from "/vite.svg";
 import "../App.css";
 import { backendUrl } from "../utils/config.defaults";
+import { Link } from "react-router-dom";
 
 function MainPage() {
   const [count, setCount] = useState(0);
@@ -43,6 +44,7 @@ function MainPage() {
         Click on the Vite and React logos to learn more
       </p>
       <p>{serverResponse}</p>
+      <Link to={"/helloworld"}>Go to HelloWorld</Link>
     </>
   );
 }
